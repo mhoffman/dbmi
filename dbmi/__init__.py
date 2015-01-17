709f1acb91c1468e05c6a5dbbd7ced1b0bca3fa7
hoffmannm <mjhoffmann@gmail.com>
2015-01-17 15:52:28 -0800

    Added dictionary merge function

--- dbmi/__init__.py
+++ dbmi/__init__.py
@@ -13,6 +13,25 @@ import numpy as np
 import scipy.signal
 import espresso.io
 
+def merge(dict1, dict2):
+    """Recursively merge two dictionaries
+    Kudos: http://stackoverflow.com/questions/7204805/dictionaries-of-dictionaries-merge
+    """
+    for k in set(dict1.keys()).union(dict2.keys()):
+        if k in dict1 and k in dict2:
+            if isinstance(dict1[k], dict) and isinstance(dict2[k], dict):
+                yield (k, dict(mergedicts(dict1[k], dict2[k])))
+            else:
+                # If one of the values is not a dict, you can't continue merging it.
+                # Value from second dict overrides one in first and we move on.
+                yield (k, dict2[k])
+                # Alternatively, replace this with exception raiser to alert you of value conflicts
+        elif k in dict1:
+            yield (k, dict1[k])
+        else:
+            yield (k, dict2[k])
+
+
 def calculate_interaction_energy(interactions, adsorbates, IR, pbc=None, verbose=False):
     if verbose:
         if pbc:
